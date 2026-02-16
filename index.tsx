8acf4316bb5ae6214f1a24130f22f2c9cd057341
zivanovicdordje <zivanovicdordje@gmail.com>
2026-02-16 20:09:53 +0100

    Nova promena

--- index.tsx
+++ index.tsx
@@ -1,9 +1,10 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App'; // Gledaj tvoj App.tsx fajl
+import './index.css';     // OBAVEZNO: Ovo povezuje tvoj CSS sa sajtom
 
 const rootElement = document.getElementById('root');
+
 if (rootElement) {
   const root = createRoot(rootElement);
   root.render(
